c620c62ad5559163d226e49f0b642cdec7b56bee
Leon Huang <royleon4@users.noreply.github.com>
2026-07-22 14:52:34 +0800

    在主應用加入語言 Provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "./lib/auth";
+import { LanguageProvider } from "./lib/i18n";
 import App from "./App";
 import "./index.css";
 
@@ -16,9 +17,11 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
+        <LanguageProvider>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </LanguageProvider>
       </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>,
