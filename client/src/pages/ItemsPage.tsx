96784537a1084e7736b9b586574375e535ef0c5a
Leon Huang <royleon4@users.noreply.github.com>
2026-07-31 07:03:48 -0700

    Show post timestamps on item wall cards

--- client/src/pages/ItemsPage.tsx
+++ client/src/pages/ItemsPage.tsx
@@ -89,7 +89,7 @@ export default function ItemsPage() {
       {posts.length > 0 && (
         <div className="mt-8 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
           {posts.map((post) => (
-            <PostCard key={post.id} post={post} />
+            <PostCard key={post.id} post={post} showCreatedAt />
           ))}
         </div>
       )}
